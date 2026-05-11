cc76c5cb3037fa7a5c7ede6ec2c4bc02a34b9db5
Hughpig <3918967988@qq.com>
2026-05-11 22:43:39 +0800

    add transform.py and watermark.py

--- core/transform.py
+++ core/transform.py
@@ -1,10 +1,25 @@
 # 变换层：实现 DWT, DCT 或 FFT
 
-def apply_dwt(image):
-    pass
+import pywt
+import numpy as np
 
-def apply_dct(image):
-    pass
+def apply_dwt(image:np.ndarray,wavelet:str='haar')->tuple:
+    # 使用 PyWavelets 进行离散小波变换
+    # 转成float32计算
+    float_image = image.astype(np.float32)
+    coeffs = pywt.dwt2(float_image, wavelet)
+    return coeffs
 
-def apply_fft(image):
-    pass
+def apply_idwt(coeffs:tuple,wavelet:str='haar')->np.ndarray:
+    # 使用 PyWavelets 进行离散小波逆变换
+    reconstructed_image = pywt.idwt2(coeffs, wavelet)
+    safe_img=np.clip(reconstructed_image, 0, 255).astype(np.uint8)
+    return safe_img
+
+def apply_dct(image:np.ndarray)->np.ndarray:
+    # 使用 NumPy 进行离散余弦变换
+    return np.fft.fft2(image)
+
+def apply_fft(image:np.ndarray)->np.ndarray:
+    # 使用 NumPy 进行快速傅里叶变换
+    return np.fft.fft2(image)
--- core/watermark.py
+++ core/watermark.py
@@ -1,7 +1,43 @@
+import numpy as np
+
 # 嵌入层：实现 QIM 量化索引调制嵌入与提取
 
-def embed_watermark(image, watermark):
-    pass
+def embed_watermark(image:np.ndarray, watermark:np.ndarray,delta:float=10.0)->np.ndarray:
+    # 利用QIM在LL子带嵌入一维水印
+    image_copy=image.copy()
+    # 展平矩阵
+    flat_image=image_copy.flatten()
+    wm_len=len(watermark)
+    if wm_len>len(flat_image):
+        raise ValueError("Watermark is too large to embed in the image.")
+    
+    target_coeffs=flat_image[:wm_len]
+    # QIM嵌入
 
-def extract_watermark(watermarked_image):
-    pass
+    even_quantized=np.round(target_coeffs/(2*delta))*2*delta
+    offset=np.where(target_coeffs>=even_quantized,delta,-delta)
+
+    # 嵌入水印
+    modified_coeffs=even_quantized+offset*watermark
+    flat_image[:wm_len]=modified_coeffs
+    # 重塑回原图像形状
+    return flat_image.reshape(image_copy.shape)
+
+def extract_watermark(watermarked_image:np.ndarray, watermark:np.ndarray, delta:float=10.0)->np.ndarray:
+    # 利用QIM在LL子带提取一维水印
+    watermarked_image_copy=watermarked_image.copy()
+    # 展平矩阵
+    flat_watermarked_image=watermarked_image_copy.flatten()
+    wm_len=len(watermark)
+    if wm_len>len(flat_watermarked_image):
+        raise ValueError("Watermark is too large to extract from the image.")
+
+    target_coeffs=flat_watermarked_image[:wm_len]
+    # QIM提取
+
+    even_quantized=np.round(target_coeffs/(2*delta))*2*delta
+    offset=np.where(target_coeffs>=even_quantized,delta,-delta)
+
+    # 提取水印
+    extracted_watermark=(target_coeffs-even_quantized)/offset
+    return extracted_watermark
